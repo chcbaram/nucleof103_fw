87cdf36d599f3e611894750e6ad9d9c56b4a23d6
chcbaram <chcbaram@paran.com>
2018-05-12 00:57:31 +0900

    led 예제 추가

---
+++ 01_led/src/ap/ap.c
@@ -0,0 +1,27 @@
+/*
+ * ap.c
+ *
+ *  Created on: 2018. 5. 12.
+ *      Author: Baram
+ */
+
+
+#include "ap.h"
+
+
+
+
+
+void apInit(void)
+{
+
+}
+
+void apMain(void)
+{
+  while(1)
+  {
+    ledToggle(0);
+    delay(500);
+  }
+}
---
+++ 01_led/src/hw/hw.c
@@ -0,0 +1,30 @@
+/*
+ * hw.c
+ *
+ *  Created on: 2018. 5. 12.
+ *      Author: Baram
+ */
+
+
+#include "hw.h"
+
+
+
+
+
+void hwInit(void)
+{
+  bspInit();
+
+  ledInit();
+}
+
+void delay(uint32_t time_ms)
+{
+  HAL_Delay(time_ms);
+}
+
+uint32_t millis(void)
+{
+  return HAL_GetTick();
+}

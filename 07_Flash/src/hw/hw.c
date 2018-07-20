ade1b2b3c5c06ef9c110a4133e146b477d306be1
chcbaram <chcbaram@paran.com>
2018-07-21 01:38:12 +0900

    Flash 예제 추가

---
+++ 07_Flash/src/hw/hw.c
@@ -0,0 +1,35 @@
+/*
+ * hw.c
+ *
+ *  Created on: 2018. 5. 12.
+ *      Author: Baram
+ */
+
+
+#include "hw.h"
+
+
+
+
+
+void hwInit(void)
+{
+  bspInit();
+
+  swtimerInit();
+  ledInit();
+  buttonInit();
+  uartInit();
+  timerInit();
+  flashInit();
+}
+
+void delay(uint32_t time_ms)
+{
+  HAL_Delay(time_ms);
+}
+
+uint32_t millis(void)
+{
+  return HAL_GetTick();
+}
